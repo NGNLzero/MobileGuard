c2cc1f0391326240cfe6565c0cdd2405d5140f60
ZGwei123 <839319250@qq.com>
2017-11-05 18:57:31 +0800

    解决添加通讯卫士功能模块时存在的错误

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
@@ -82,17 +82,17 @@ public class BlackContactAdapter extends BaseAdapter{
         holder.mContactImgv.setBackgroundResource(R.drawable.brightpurple_contact_icon);
         holder.mDeleteView.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View  view){
+            public void onClick(View  view) {
                 boolean datele = dao.detele(contactInfos.get(i));
-                if(datele){
+                if (datele) {
                     contactInfos.remove(contactInfos.get(i));
                     BlackContactAdapter.this.notifyDataSetChanged();
                     // 如果数据库中没有数据了，则执行回调函数
-                    if(dao.getTotalNumber() == 0){
+                    if (dao.getTotalNumber() == 0) {
                         callBack.DataSizeChanged();
-                    } else{
-                        Toast.makeText(context, "删除失败！", Toast.LENGTH_LONG).show();
                     }
+                } else {
+                    Toast.makeText(context, "删除失败！", Toast.LENGTH_LONG).show();
                 }
             }
         });
